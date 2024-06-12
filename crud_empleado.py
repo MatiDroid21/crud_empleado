a8c339a0795250cb67f471694a24343258bec0e6
MatiDroid21 <120347956+MatiDroid21@users.noreply.github.com>
2024-06-11 21:33:49 -0400

    python con el profe Ernesto
    Muchas Gracias Profe :)

---
+++ crud_empleado.py
@@ -0,0 +1,12 @@
+import os
+import json
+
+os.system("cls")
+
+def cargar_json(url_archivo):
+    with open(url_archivo, "r") as archivo:
+        return json.load(archivo)
+    
+empleados = cargar_json("crud/empleados.json") #! nombre del archivo json a importar
+print(empleados)
+
